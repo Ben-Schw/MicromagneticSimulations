4719a1326cbaf14b5d49df623c7895c10b4c2244
Ben-Schw <ben.schwedel@gmail.com>
2025-12-18 16:07:28 +0100

    changing names and fixing bugs in 2D; finishing first build in 3D

--- Monte_carlo/magnetism_2d_discrete.c
+++ Monte_carlo/magnetism_2d_discrete.c
@@ -9,19 +9,19 @@
 #define gn 1.25
 #define nuinvers 1
 #define J 1.0
-#define B 0.1
+#define B 0.0
 
 int lattice[latticeSize][latticeSize];
 
 int fillLattice(void);
-int Metropolis(double temp);
-double Hamiltonian(int i, int j);
-double Magnetisierung(void);
-double Suszeptibilitaet(double temp, double wurzel, double magsquared);
-double NeelVector(void);
+int metropolis(double temp);
+double hamiltonian(int i, int j);
+double magnetisation(void);
+double susceptibility(double temp, double wurzel, double magsquared);
+double neelVector(void);
 void write(double limit, FILE *fp, double temp);
-int GetLower(int index);
-int GetHigher(int index);
+int getLower(int index);
+int getHigher(int index);
 
 /**** Initialize lattice ****/
 int fillLattice(void) {
@@ -34,12 +34,11 @@ int fillLattice(void) {
 }
 
 /**** Metropolis algorithm (one sweep in fixed i,j order) ****/
-int Metropolis(double temp) {
+int metropolis(double temp) {
     for (int i = 0; i < latticeSize; i++) {
         for (int j = 0; j < latticeSize; j++) {
-            double dH = Hamiltonian(i, j);
+            double dH = hamiltonian(i, j);
 
-            // Keep acceptance rule exactly as requested (your "point 4")
             if (rand() < RAND_MAX * exp(-dH / temp)) {
                 lattice[i][j] = -1 * lattice[i][j];
             }
@@ -52,80 +51,83 @@ int Metropolis(double temp) {
 void write(double limit, FILE *fp, double temp) {
     double mean_m2 = 0.0;      // <m^2> with m = |magnetization per spin|
     double mean_m = 0.0;       // <m>
-    double mean_neel = 0.0;    // <|Neel|>
+    double mean_abs_m = 0.0;   // <|m|> 
+
+    double mean_neel = 0.0;    // <Neel>
+    double mean_neel_abs = 0.0; // <|Neel|>
+
 
     for (double i = 0; i < limit; i += 1.0) {
-        Metropolis(temp);
+        metropolis(temp);
 
         // measurement after thermalization
         if (i >= 0.7 * limit) {
-            double m = Magnetisierung();
-            mean_m2   += (m * m) / (0.3 * limit);
-            mean_m    += m / (0.3 * limit);
-            mean_neel += NeelVector() / (0.3 * limit);
+            double m  = magnetisation();
+            mean_m  += m / (0.3 * limit);
+            mean_m2 += (m * m) / (0.3 * limit);
+            mean_abs_m += fabs(m) / (0.3 * limit);
+
+            mean_neel += neelVector() / (0.3 * limit);
+            mean_neel_abs += fabs(neelVector()) / (0.3 * limit);
         }
     }
 
     double m_mean_sq = pow(mean_m, 2.0);        // <m>^2
-    double neel_sq   = pow(mean_neel, 2.0);     // <|Neel|>^2
+    double neel_sq   = pow(mean_neel, 2.0);     // <Neel>^2
 
-    double sus = Suszeptibilitaet(temp, mean_m2, m_mean_sq);
-    double sqrt_m2 = sqrt(mean_m2);
+    double sus = susceptibility(temp, mean_m2, m_mean_sq);
+    double sqrt_m_squared = sqrt(mean_m2);
 
-    // CSV-like output (no trailing comma)
+    //output
     fprintf(fp, "%g, %g, %g, %g, %g, %g, %g, %g, %g\n",
             -(temp - 2.269) / 2.269,
             B,
-            m_mean_sq,
-            sqrt_m2,
+            mean_abs_m,
+            sqrt_m_squared,
             sus,
             pow(latticeSize, bn),
             pow(latticeSize, gn),
             pow(latticeSize, nuinvers),
-            neel_sq);
+            mean_neel_abs);
 }
 
-/**** Energy change for flipping spin (i,j): ΔE = 2 J s_ij * sum_nn s_nn + B * s_ij ****/
-double Hamiltonian(int i, int j) {
+/**** Energy change for flipping spin (i,j): ΔE = 2 J s_ij * sum_nn s_nn + 2 B s_ij ****/
+double hamiltonian(int i, int j) {
     double ham = (double)2 * J * lattice[i][j] *
-                 (lattice[GetLower(i)][j] + lattice[GetHigher(i)][j] +
-                  lattice[i][GetLower(j)] + lattice[i][GetHigher(j)]) + (double) B * lattice[i][j];
+                 (lattice[getLower(i)][j] + lattice[getHigher(i)][j] +
+                  lattice[i][getLower(j)] + lattice[i][getHigher(j)]) + (double) 2 * B * lattice[i][j];
     return ham;
 }
 
 /**** Periodic boundary: lower neighbor index ****/
-int GetLower(int index) {
+int getLower(int index) {
     if (index == 0) return latticeSize - 1;
     return index - 1;
 }
 
 /**** Periodic boundary: higher neighbor index ****/
-int GetHigher(int index) {
+int getHigher(int index) {
     if (index == latticeSize - 1) return 0;
     return index + 1;
 }
 
-/**** Magnetization per spin (absolute value) ****/
-double Magnetisierung(void) {
+/**** Magnetisation per spin (absolute value) ****/
+double magnetisation(void) {
     double mag = 0.0;
-
-    for (int i = 0; i < latticeSize; i++) {
-        for (int j = 0; j < latticeSize; j++) {
-            mag += (1.0 / (latticeSize * latticeSize)) * lattice[i][j];
-        }
-    }
-
-    return fabs(mag);
+    for (int i = 0; i < latticeSize; i++)
+        for (int j = 0; j < latticeSize; j++)
+            mag += lattice[i][j];
+    return mag / (double)(latticeSize * latticeSize);
 }
 
 /**** Susceptibility using χ = N/T * ( <m^2> - <m>^2 ), with m per spin ****/
-double Suszeptibilitaet(double temp, double mean_m2, double mean_m_sq) {
+double susceptibility(double temp, double mean_m2, double mean_m_sq) {
     double sus = pow((double)latticeSize, 2.0) / temp * (mean_m2 - mean_m_sq);
     return sus;
 }
 
 /**** Neel (staggered) order parameter per spin (absolute value) ****/
-double NeelVector(void) {
+double neelVector(void) {
     double neel = 0.0;
 
     for (int i = 0; i < latticeSize; i++) {
@@ -135,16 +137,16 @@ double NeelVector(void) {
         }
     }
 
-    return fabs(neel);
+    return neel;
 }
 
 int main(void) {
     srand((unsigned)time(NULL));
 
-    FILE *f = fopen("QuelleLatticeSize128f.txt", "w");
+    FILE *f = fopen("2dLatticeSize16.csv", "w");
     if (!f) { perror("fopen"); return 1; }
 
-    fprintf(f, "T, B, <m>^2, sqrt(<m^2>), chi, L^b, L^g, L^(1/nu), <|Neel|>^2\n");
+    fprintf(f, "T_reduced, B, <m>^2, sqrt(<m^2>), chi, L^b, L^g, L^(1/nu), <|Neel|>^2\n");
 
     fillLattice();
 
--- Monte_carlo/magnetism_3d_continuous.c
+++ Monte_carlo/magnetism_3d_continuous.c
@@ -1,4 +1,4 @@
-/**** Monte Carlo simulation of the 3D Ising model (anti/ferromagnet) with periodic boundary conditions ****/
+/**** Monte Carlo simulation of the 3D Heisenberg model (anti/ferromagnet) with periodic boundary conditions ****/
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -6,31 +6,126 @@
 
 #define latticeSize 16
 #define J 1.0
+#define M_PI 3.14159265358979323846
 
+/**** simple 3D vector structure ****/
 typedef struct {
     double x;
     double y;
     double z;
 } vector3;
 
+/**** basic vector algebra ****/
+double dot(vector3 a, vector3 b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
 vector3 norm(vector3 v) {
-    double length = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    double length = sqrt(dot(v, v));
     vector3 normalized = {v.x / length, v.y / length, v.z / length};
     return normalized;
 }
 
-vector3 lattice[latticeSize][latticeSize][latticeSize];
+/*** generate random unit vector ****/
+vector3 random_unit_vector() {
+    double theta = ((double)rand() / RAND_MAX) * 2.0 * M_PI;
+    double phi = acos(1.0 - 2.0 * ((double)rand() / RAND_MAX));
+    vector3 v = {sin(phi) * cos(theta), sin(phi) * sin(theta), cos(phi)};
+    return v;
+}
+
+/*** 3D Lattice and magnetic field vector ***/
+vector3 lattice[latticeSize][latticeSize][latticeSize]; // 3D lattice of spins
+
+const vector3 B = {0.0, 0.0, 0.1}; // External magnetic field
 
 /**** Periodic boundary: small neighbor index ****/
-int GetMinus(int index) {
+int getMinus(int index) {
     if (index == 0) return latticeSize - 1;
     return index - 1;
 }
 
 /**** Periodic boundary: high neighbor index ****/
-int GetPlus(int index) {
+int getPlus(int index) {
     if (index == latticeSize - 1) return 0;
     return index + 1;
+}
+
+/**** Magnetic Quantities****/
+vector3 magentisation(void) {
+    vector3 mag = {0.0, 0.0, 0.0};
+
+    for (int i = 0; i < latticeSize; i++) {
+        for (int j = 0; j < latticeSize; j++) {
+            for (int k = 0; k < latticeSize; k++) {
+                mag.x += (1.0 / (latticeSize * latticeSize *latticeSize)) * lattice[i][j][k].x;
+                mag.y += (1.0 / (latticeSize * latticeSize *latticeSize)) * lattice[i][j][k].y;
+                mag.z += (1.0 / (latticeSize * latticeSize *latticeSize)) * lattice[i][j][k].z;
+            }
+        }
+    }
+
+    return mag;
+}
+
+vector3 neelVector(void) {
+    vector3 neel = {0.0, 0.0, 0.0};
+
+    for (int i = 0; i < latticeSize; i++) {
+        for (int j = 0; j < latticeSize; j++) {
+            for (int k = 0; k < latticeSize; k++) {
+                int stagger = ((i + j + k) % 2 == 0) ? 1 : -1;
+                neel.x += (1.0 / (latticeSize * latticeSize *latticeSize)) * lattice[i][j][k].x * (double)stagger;
+                neel.y += (1.0 / (latticeSize * latticeSize *latticeSize)) * lattice[i][j][k].y * (double)stagger;
+                neel.z += (1.0 / (latticeSize * latticeSize *latticeSize)) * lattice[i][j][k].z * (double)stagger;
+            }
+        }
+    }
+
+    return neel;
+}
+
+/**** helper functions for simulation****/
+vector3 nn_sum(int i, int j, int k){
+    vector3 s = {0.0, 0.0, 0.0};
+    vector3 a = {0.0, 0.0, 0.0};
+
+    a = lattice[getMinus(i)][j][k];  s.x += a.x; s.y += a.y; s.z += a.z;
+    a = lattice[getRight(i)][j][k]; s.x += a.x; s.y += a.y; s.z += a.z;
+    a = lattice[i][getMinus(j)][k];  s.x += a.x; s.y += a.y; s.z += a.z;
+    a = lattice[i][getRight(j)][k]; s.x += a.x; s.y += a.y; s.z += a.z;
+    a = lattice[i][j][getMinus(k)];  s.x += a.x; s.y += a.y; s.z += a.z;
+    a = lattice[i][j][getRight(k)]; s.x += a.x; s.y += a.y; s.z += a.z;
+
+    return s;
+}
+
+/**** Hamiltonian difference ****/
+double hamiltonian(int i, int j, int k, vector3 S_new) {
+    vector3 S_old = lattice[i][j][k];
+    vector3 sum_nn = nn_sum(i, j, k);
+    vector3 diff = {S_new.x - S_old.x, S_new.y - S_old.y, S_new.z - S_old.z};
+
+    double dH = -J * (dot(S_new, sum_nn) - dot(S_old, sum_nn)) - dot(B, diff);
+    return dH;
+}
+
+/**** Metropolis algorithm  ****/
+int metropolis(double temp) {
+    for (int i = 0; i < latticeSize; i++) {
+        for (int j = 0; j < latticeSize; j++) {
+            for (int k = 0; k < latticeSize; k++) {
+
+                vector3 S_new = random_unit_vector();
+                double dH = hamiltonian(i, j, k, S_new);
+
+                if (rand() < RAND_MAX * exp(-dH / temp)) {
+                    lattice[i][j][k] = S_new;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 /**** Initialize lattice ****/
@@ -44,7 +139,67 @@ void fillLattice(void) {
     }
 }
 
+/**** perform steps and write into output  ****/
+void mc_steps(double temp, int limit, FILE *fp) {
+    vector3 mag = {0.0, 0.0, 0.0};
+    vector3 neel = {0.0, 0.0, 0.0};
+    // thermalization and measurement
+    for (int i = 0; i < limit; i++) {
+        metropolis(temp);
 
+        // after thermalization -> measure
+        if (i >= 0.7 * limit) {
+            vector3 m = magentisation();
+            mag.x += m.x / (0.3 * limit);
+            mag.y += m.y / (0.3 * limit);
+            mag.z += m.z / (0.3 * limit);
+
+            vector3 n = neelVector();
+            neel.x += n.x / (0.3 * limit);
+            neel.y += n.y / (0.3 * limit);
+            neel.z += n.z / (0.3 * limit);
+        }
+    }
+    // write output
+    fprintf(fp, "%g, %g, %g, %g, %g, %g, %g, %g, %g, %g\n",
+            temp,
+            B.x,
+            B.y,
+            B.z,
+            mag.x,
+            mag.y,
+            mag.z,
+            neel.x,
+            neel.y,
+            neel.z);
+}
+
+/**** main simulation ****/
 int main(void) {
+    srand((unsigned)time(NULL));
+
+    // open output file
+    FILE *f = fopen("3dLatticeSize16.csv", "w");
+    if (!f) { perror("fopen"); return 1; }
+
+    fprintf(f, "T, B_x, B_y, B_z, S_x, S_y, S_z, N_x, N_y, N_z\n");
+
+    fillLattice();
+
+    // simulation for several temperatures
+    double temp;
+    for (temp = 0.0; temp <= 4.0; temp += 0.02) {
+
+        if (temp <= 2.0) {
+            mc_steps(temp, 1000, f);
+        } else if (temp < 2.4) {
+            mc_steps(temp, 100000, f);
+        } else {
+            mc_steps(temp, 10000, f);
+        }
+
+    }
+
+    fclose(f);
     return 0;
 }
